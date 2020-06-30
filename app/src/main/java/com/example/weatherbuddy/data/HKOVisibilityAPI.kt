73751982957bc64fe2132deb9225969bc4abe2cd
Nelson Chan <chakflying@hotmail.com>
2020-06-30 22:49:13 +0800

    Basic navigation and UI

---
+++ app/src/main/java/com/example/weatherbuddy/data/HKOVisibilityAPI.kt
@@ -0,0 +1,4 @@
+package com.example.weatherbuddy.data
+
+interface HKOVisibilityAPI {
+}
---
+++ app/src/main/java/com/example/weatherbuddy/data/HKOVisibilityResponse.kt
@@ -0,0 +1,6 @@
+package com.example.weatherbuddy.data
+
+data class HKOVisibilityResponse(
+    val `data`: List<List<String>>,
+    val fields: List<String>
+)
